4613f153c3be0cb8f088d5cb006b99610c278f21
BaziotaBeans <1000017079@ucan.edu>
2026-01-10 23:02:00 +0100

    FIRST COMMIT

---
+++ api/server.js
@@ -0,0 +1,16 @@
+// server.cjs (na raiz)
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3333;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
+
+// Export the Server API
+module.exports = server;
